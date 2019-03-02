8c5de8568e06b52f0201dd0bd84e385e9c62d2ea
Young Liu <youngliu92@gmail.com>
2019-03-02 12:33:53 -0800

    separate logic into two controllers.

--- WordCounter/Controllers/HomeController.cs
+++ WordCounter/Controllers/HomeController.cs
@@ -1,29 +1,12 @@
 using Microsoft.AspNetCore.Mvc;
-using WordCounter.Models;
 
 namespace WordCounter.Controllers
 {
   public class HomeController : Controller
   {
-    [HttpGet("/message")]
-    public ActionResult Message() {
-      return View();
-    }
-
     [HttpGet("/")]
     public ActionResult Index(){
       return View();
-    }
-
-    [HttpGet("/form")]
-    public ActionResult Form() {
-      return View();
-    }
-
-    [HttpPost("/newCounter")]
-    public ActionResult newCounter(string word, string sentence){
-      RepeatCounter rc = new RepeatCounter(word, sentence);
-      return View("Message", rc);
     }
   }
 }
---
+++ WordCounter/Controllers/WordCounterController.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+using WordCounter.Models;
+
+namespace WordCounter.Controllers
+{
+  public class WordCounterController : Controller
+  {
+    [HttpGet("/message")]
+    public ActionResult Message() {
+      return View();
+    }
+
+    [HttpGet("/form")]
+    public ActionResult Form() {
+      return View();
+    }
+
+    [HttpPost("/newCounter")]
+    public ActionResult newCounter(string word, string sentence){
+      RepeatCounter rc = new RepeatCounter(word, sentence);
+      return View("Message", rc);
+    }
+  }
+}
