8ed25647d5dffdcf34f0a334864f044b59bc453f
Young Liu <youngliu92@gmail.com>
2019-03-01 20:55:38 -0800

    add HomeController.cs to the controller folder.

---
+++ WordCounter/Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace WordCounter.Controllers
+{
+  public class HomeController : Controller
+  {
+
+    public string Hello() { return "Hello friend!"; }
+
+  }
+}
