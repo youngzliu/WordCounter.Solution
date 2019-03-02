0dfed78bd0ade0c0e271547307699c9dc6618159
Young Liu <youngliu92@gmail.com>
2019-03-02 00:29:20 -0800

    use razor engine to show result of model.

--- WordCounter/Controllers/HomeController.cs
+++ WordCounter/Controllers/HomeController.cs
@@ -1,11 +1,14 @@
 using Microsoft.AspNetCore.Mvc;
+using WordCounter.Models;
 
 namespace WordCounter.Controllers
 {
   public class HomeController : Controller
   {
     [Route("/")]
-    public ActionResult index() { return View(); }
-
+    public ActionResult message() {
+      RepeatCounter rc = new RepeatCounter("fire", "fire fire bob bob FIRE.");
+      return View(rc);
+    }
   }
 }
