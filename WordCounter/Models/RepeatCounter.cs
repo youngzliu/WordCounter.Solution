f19811871efb5d95f595789d1b457aec5976c676
Young Liu <youngliu92@gmail.com>
2019-03-02 00:19:11 -0800

    update namespace of RepeatCounter.cs

--- WordCounter/Models/RepeatCounter.cs
+++ WordCounter/Models/RepeatCounter.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Collections.Generic;
 
-namespace WordCounter{
+namespace WordCounter.Models{
   public class RepeatCounter{
     private string MyWord;
     private string[] MySentence;
